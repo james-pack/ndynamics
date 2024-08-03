f61ca331de74e0d8b567d77d79227ef272b98fe6
James Pack <jgrimm@gmail.com>
2024-08-03 17:10:16 -0400

    Added method to count the number of bits set in an integer.

--- base/bits.h
+++ base/bits.h
@@ -3,9 +3,9 @@
 #include <cstdint>
 
 #ifdef __has_include
-#  if __has_include(<bit>)
-#    include <bit>
-#  endif
+#if __has_include(<bit>)
+#include <bit>
+#endif
 #endif
 
 namespace ndyn {
@@ -22,6 +22,21 @@ inline constexpr std::size_t number_of_bits(std::size_t x) noexcept {
 }
 #endif
 
+// If possible, we want to use the std method for computing the number of bits in a value, but it is
+// only available in C++20.
+#if __cpp_lib_int_pow2 >= 202002L
+inline constexpr std::size_t bit_count(std::size_t x) noexcept { return std::popcount(x); }
+#else
+inline constexpr std::size_t bit_count(std::size_t x) noexcept {
+  std::size_t result{};
+  while (x > 0) {
+    ++result;
+    x = x & (x - 1);
+  }
+  return result;
+}
+#endif
+
 }  // namespace internal
 
 /**
@@ -29,6 +44,13 @@ inline constexpr std::size_t number_of_bits(std::size_t x) noexcept {
  */
 inline constexpr uint8_t bit_width(std::size_t value) {
   return static_cast<uint8_t>(internal::number_of_bits(value));
+}
+
+/**
+ * Compute the number of bits set in the given value.
+ */
+inline constexpr uint8_t bit_count(std::size_t value) {
+  return static_cast<uint8_t>(internal::bit_count(value));
 }
 
 }  // namespace ndyn
--- base/bits_test.cc
+++ base/bits_test.cc
@@ -23,4 +23,20 @@ TEST(BitWidthTest, VerifyAssortedValues) {
   EXPECT_EQ(11, bit_width(1024));
 }
 
+TEST(BitCountTest, ZeroHasZeroBitsSet) { EXPECT_EQ(0, bit_count(0)); }
+
+TEST(BitCountTest, OneHasOneBitSet) { EXPECT_EQ(1, bit_count(1)); }
+TEST(BitCountTest, TwoHasOneBitSet) { EXPECT_EQ(1, bit_count(2)); }
+TEST(BitCountTest, ThreeHasTwoBitsSet) { EXPECT_EQ(2, bit_count(3)); }
+
+TEST(BitCountTest, VerifyAssortedValues) {
+  EXPECT_EQ(1, bit_count(64));
+  EXPECT_EQ(2, bit_count(65));
+  EXPECT_EQ(7, bit_count(127));
+  EXPECT_EQ(1, bit_count(128));
+  EXPECT_EQ(8, bit_count(255));
+  EXPECT_EQ(10, bit_count(1023));
+  EXPECT_EQ(1, bit_count(1024));
+}
+
 }  // namespace ndyn
