3ef797750f833763412c837feff7be08736185de
James Pack <jgrimm@gmail.com>
2024-08-18 06:37:55 -0400

    Added STA tests for left contraction

--- math/multivector_test.cc
+++ math/multivector_test.cc
@@ -319,4 +319,62 @@ TEST(MultivectorTest, CanDoLeftContractionOnVga) {
   EXPECT_EQ(u * v + +4.f - 4.f * x, v.left_contraction(u * v));
 }
 
+TEST(MultivectorTest, CanDoLeftContractionOnSimpleSpacetime) {
+  static constexpr auto x{SpacetimeMultivector<float>::e<1>()};
+  static constexpr auto a{SpacetimeMultivector<float>{1.f}};  // 1
+  static constexpr auto u{1.f + x};                           // 1 + x
+  static constexpr auto v{u * u};                             // (1 + x)^2 = 2x
+  static constexpr auto w{v * v};                             // (2x)^2 = -4
+
+  EXPECT_EQ(-1.f, x.left_contraction(x));
+  EXPECT_EQ(1.f, a.left_contraction(a));
+  EXPECT_EQ(-1.f, x.left_contraction(u));
+  EXPECT_EQ(x - 1.f, u.left_contraction(x));
+  EXPECT_EQ(u - 1.f, u.left_contraction(u));
+  EXPECT_EQ(-2.f, v.left_contraction(x));
+  EXPECT_EQ(-2.f, x.left_contraction(v));
+  EXPECT_EQ(-2.f, v.left_contraction(u));
+  EXPECT_EQ(0.f, v.left_contraction(w));
+  EXPECT_EQ(-8.f * x, w.left_contraction(v));
+  EXPECT_EQ(-4.f - 4.f * x, w.left_contraction(u));
+  EXPECT_EQ(-4.f, u.left_contraction(w));
+}
+
+TEST(MultivectorTest, CanDoLeftContractionOnSpacetime) {
+  static constexpr auto t{SpacetimeMultivector<float>::e<0>()};
+  static constexpr auto x{SpacetimeMultivector<float>::e<1>()};
+  static constexpr auto y{SpacetimeMultivector<float>::e<2>()};
+  static constexpr auto z{SpacetimeMultivector<float>::e<3>()};
+  static constexpr auto a{SpacetimeMultivector<float>{1.f}};
+
+  static constexpr auto r{1.f + t};
+  static constexpr auto u{1.f + x};
+  static constexpr auto v{1.f + 2.f * y};
+  static constexpr auto w{2.f + 3.f * z};
+
+  EXPECT_EQ(u, r.left_contraction(u));
+  EXPECT_EQ(0.f, t.left_contraction(u));
+  EXPECT_EQ(v, u.left_contraction(v));
+  EXPECT_EQ(w, v.left_contraction(w));
+  EXPECT_EQ(2.f * u, w.left_contraction(u));
+
+  // Document the values of these products, since they will be used in more complicated tests below.
+  ASSERT_EQ(1.f + x + 2.f * y + 2.f * x * y, u * v);
+  ASSERT_EQ(2.f + 4.f * y + 3.f * z + 6.f * y * z, v * w);
+  ASSERT_EQ(2.f + 2.f * x + 3.f * z - 3.f * x * z, w * u);
+
+  // In these three cases, the lhs of the left contraction has a constant plus a term that is
+  // orthogonal to all the components of the rhs. So, all of the results will be that constant times
+  // the rhs.
+  EXPECT_EQ(v * w, u.left_contraction(v * w)) << "v * w: " << v * w;
+  EXPECT_EQ(w * u, v.left_contraction(w * u));
+  EXPECT_EQ(2.f * u * v, w.left_contraction(u * v));
+
+  EXPECT_EQ(4.f + 8.f * y + 6.f * z + 12.f * y * z + -9.f + 18.f * y, w.left_contraction(v * w));
+
+  EXPECT_EQ(4.f + 4.f * x + 6.f * z - 6.f * x * z + -9.f - 9.f * x, w.left_contraction(w * u));
+
+  EXPECT_EQ(u * v - 4.f + 4.f * x, v.left_contraction(u * v));
+}
+
 }  // namespace ndyn::math
