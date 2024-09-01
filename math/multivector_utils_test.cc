9758afdcfe85f4ddeee23412db399042348ce78b
James Pack <jgrimm@gmail.com>
2024-09-01 18:09:07 -0400

    Added tests for the square_magnitude of a Multivector and the abs.

--- math/multivector_utils_test.cc
+++ math/multivector_utils_test.cc
@@ -1,9 +1,67 @@
 #include "math/multivector_utils.h"
 
+#include <cmath>
+
 #include "gtest/gtest.h"
 #include "math/multivector.h"
 
 namespace ndyn::math {
+
+TEST(MagnitudeTest, CanComputeSquareMagnitude) {
+  using ScalarType = float;
+  using MultivectorT = VgaMultivector<ScalarType>;
+
+  static constexpr MultivectorT x = MultivectorT::template e<0>();
+  static constexpr MultivectorT y = MultivectorT::template e<1>();
+  static constexpr MultivectorT z = MultivectorT::template e<2>();
+
+  // The square magnitude of a scalar is just that value squared.
+  EXPECT_EQ(1, square_magnitude(MultivectorT{1.f}));
+  EXPECT_EQ(1, square_magnitude(MultivectorT{-1.f}));
+  EXPECT_EQ(9, square_magnitude(MultivectorT{3.f}));
+  EXPECT_EQ(9, square_magnitude(MultivectorT{-3.f}));
+
+  // Vectors give the length of the vector squared.
+  EXPECT_EQ(2, square_magnitude(x + y));
+  EXPECT_EQ(3, square_magnitude(x + y + z));
+  EXPECT_EQ(5, square_magnitude(x + 2.f * y));
+  EXPECT_EQ(30, square_magnitude(x + 2.f * y + 5.f * z));
+
+  // Bivectors and trivectors in the VGA give negative square magnitudes
+  EXPECT_EQ(-9, square_magnitude(3.f * x * z));
+  EXPECT_EQ(-25, square_magnitude(5.f * x * y * z));
+  EXPECT_EQ(-4, square_magnitude(x + 2.f * y + 3.f * x * z));
+  EXPECT_EQ(-4, square_magnitude(x + 2.f * y + 3.f * x * y * z));
+  EXPECT_EQ(-1.f * (-3.f * -3.f), square_magnitude(-3.f * x * z));
+  EXPECT_EQ(-1.f * (-5.f * -5.f), square_magnitude(-5.f * x * y * z));
+}
+
+TEST(MagnitudeTest, CanComputeAbs) {
+  using std::sqrt;
+
+  using ScalarType = float;
+  using MultivectorT = VgaMultivector<ScalarType>;
+
+  static constexpr ScalarType EPSILON{1e-6};
+
+  static constexpr MultivectorT x = MultivectorT::template e<0>();
+  static constexpr MultivectorT y = MultivectorT::template e<1>();
+  static constexpr MultivectorT z = MultivectorT::template e<2>();
+
+  // Scalars
+  EXPECT_NEAR(1, abs(MultivectorT{1.f}), EPSILON);
+  EXPECT_NEAR(1, abs(MultivectorT{-1.f}), EPSILON);
+  EXPECT_NEAR(3, abs(MultivectorT{3.f}), EPSILON);
+  EXPECT_NEAR(3, abs(MultivectorT{-3.f}), EPSILON);
+
+  // Vectors give the length of the vector.
+  EXPECT_NEAR(sqrt(2.f), abs(x + y), EPSILON);
+  EXPECT_NEAR(sqrt(3.f), abs(x + y + z), EPSILON);
+  EXPECT_NEAR(sqrt(5.f), abs(x + 2.f * y), EPSILON);
+  EXPECT_NEAR(sqrt(30.f), abs(x + 2.f * y + 5.f * z), EPSILON);
+
+  // Bivectors and trivectors in the VGA do not have defined abs() values.
+}
 
 TEST(DecomposeTest, CanDecomposeTrivialGrade1Multivector) {
   using ScalarType = float;
