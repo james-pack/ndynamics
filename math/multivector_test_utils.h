319fbc6315d1c771c3e62a0fc1e9f955408de319
James Pack <jgrimm@gmail.com>
2024-09-02 17:39:13 -0400

    Added text feedback when test fails

--- math/multivector_test_utils.h
+++ math/multivector_test_utils.h
@@ -2,6 +2,7 @@
 
 #include <cmath>
 #include <cstring>
+#include <string>
 
 #include "gtest/gtest.h"
 #include "math/multivector.h"
@@ -12,11 +13,14 @@ template <typename MultivectorT>
 ::testing::AssertionResult AreNear(const MultivectorT& lhs, const MultivectorT& rhs,
                                    typename MultivectorT::ScalarType epsilon) {
   using std::abs;
+  using std::to_string;
   const auto difference{lhs - rhs};
   epsilon = abs(epsilon);
   for (size_t i = 0; i < MultivectorT::component_count(); ++i) {
     if (abs(difference.component(i)) > epsilon) {
-      return ::testing::AssertionFailure();
+      return ::testing::AssertionFailure()
+             << "lhs: " << lhs << ", rhs: " << rhs << ", difference: " << difference
+             << " (epsilon: " << to_string(epsilon) << ")";
     }
   }
   return ::testing::AssertionSuccess();
