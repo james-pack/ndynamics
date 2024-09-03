15e5124e9b0bb88699e5481f5d79e241d85ae12b
James Pack <jgrimm@gmail.com>
2024-09-02 22:34:16 -0400

    Added a general purpose Runge-Kutta 2 implementation.

--- math/integrators.h
+++ math/integrators.h
@@ -11,10 +11,10 @@ template <typename T, size_t SIZE>
 using UpdateFn = std::function<StateT<T, SIZE>(const StateT<T, SIZE>&)>;
 
 /**
- * Implementations of the forward Euler algorithm for integrating the state of a system according to
+ * Implementation of the forward Euler algorithm for integrating the state of a system according to
  * a differential equation.
  *
- * These implementations assume that the state contains elements that are derivatives of each other.
+ * This implementation assumes that the state contains elements that are derivatives of each other.
  * The nth element can be approximated by multiplying the n+1st element by the interval.
  *
  * The state is designed so that elements of the previous state can be updated before each step.
@@ -30,7 +30,7 @@ class ForwardEuler final {
   ForwardEuler() = default;
   ForwardEuler(UpdateFn<T, SIZE>&& updater) : updater_(std::forward<UpdateFn<T, SIZE>>(updater)) {}
 
-  StateT<T, SIZE> operator()(ScalarT interval, const StateT<T, SIZE>& s0) {
+  StateT<T, SIZE> operator()(ScalarT interval, StateT<T, SIZE> s0) {
     auto s1{s0};
     if (updater_) {
       s1 = updater_(s1);
@@ -43,21 +43,22 @@ class ForwardEuler final {
 };
 
 template <typename ScalarT, typename T>
-class ForwardEuler<ScalarT, T, 2> final {
+class ForwardEuler<ScalarT, T, 4> final {
  private:
-  UpdateFn<T, 2> updater_{};
+  UpdateFn<T, 4> updater_{};
 
  public:
   ForwardEuler() = default;
-  ForwardEuler(UpdateFn<T, 2>&& updater) : updater_(std::forward<UpdateFn<T, 2>>(updater)) {}
+  ForwardEuler(UpdateFn<T, 4>&& updater) : updater_(std::forward<UpdateFn<T, 4>>(updater)) {}
 
-  StateT<T, 2> operator()(ScalarT interval, const StateT<T, 2>& s0) {
-    auto s1{s0};
+  StateT<T, 4> operator()(ScalarT interval, StateT<T, 4> s0) {
     if (updater_) {
-      s1 = updater_(s1);
+      s0 = updater_(s0);
     }
-    s1.template set_element<0>(s1.template element<0>() + interval * s1.template element<1>());
-    return s1;
+    s0.template set_element<2>(s0.template element<2>() + interval * s0.template element<3>());
+    s0.template set_element<1>(s0.template element<1>() + interval * s0.template element<2>());
+    s0.template set_element<0>(s0.template element<0>() + interval * s0.template element<1>());
+    return s0;
   }
 };
 
@@ -70,14 +71,164 @@ class ForwardEuler<ScalarT, T, 3> final {
   ForwardEuler() = default;
   ForwardEuler(UpdateFn<T, 3>&& updater) : updater_(std::forward<UpdateFn<T, 3>>(updater)) {}
 
-  StateT<T, 3> operator()(ScalarT interval, const StateT<T, 3>& s0) {
-    auto s1{s0};
+  StateT<T, 3> operator()(ScalarT interval, StateT<T, 3> s0) {
     if (updater_) {
-      s1 = updater_(s1);
+      s0 = updater_(s0);
     }
-    s1.template set_element<1>(s1.template element<1>() + interval * s1.template element<2>());
-    s1.template set_element<0>(s1.template element<0>() + interval * s1.template element<1>());
-    return s1;
+    s0.template set_element<1>(s0.template element<1>() + interval * s0.template element<2>());
+    s0.template set_element<0>(s0.template element<0>() + interval * s0.template element<1>());
+    return s0;
+  }
+};
+
+template <typename ScalarT, typename T>
+class ForwardEuler<ScalarT, T, 2> final {
+ private:
+  UpdateFn<T, 2> updater_{};
+
+ public:
+  ForwardEuler() = default;
+  ForwardEuler(UpdateFn<T, 2>&& updater) : updater_(std::forward<UpdateFn<T, 2>>(updater)) {}
+
+  StateT<T, 2> operator()(ScalarT interval, StateT<T, 2> s0) {
+    if (updater_) {
+      s0 = updater_(s0);
+    }
+    s0.template set_element<0>(s0.template element<0>() + interval * s0.template element<1>());
+    return s0;
+  }
+};
+
+/**
+ * Implementation of the Runge-Kutta 2 algorithm for integrating the state of a system according to
+ * a differential equation.
+ *
+ * This implementation assumes that the state contains elements that are derivatives of each other.
+ * The nth element can be approximated by multiplying the n+1st element by the interval.
+ *
+ * The state is designed so that elements of the previous state can be updated before each step.
+ * This design supports approximating various differential equations, including harmonic
+ * oscillators, pendulum motion, etc.
+ */
+template <typename ScalarT, typename T, size_t SIZE>
+class RungeKutta2 final {
+ private:
+  UpdateFn<T, SIZE> updater_{};
+
+ public:
+  RungeKutta2() = default;
+  RungeKutta2(UpdateFn<T, SIZE>&& updater) : updater_(std::forward<UpdateFn<T, SIZE>>(updater)) {}
+
+  StateT<T, SIZE> operator()(ScalarT interval, StateT<T, SIZE> s0) {
+    if (updater_) {
+      s0 = updater_(s0);
+    }
+    StateT<T, SIZE> s1{s0};
+
+    for (size_t i = SIZE - 1; i > 0; --i) {
+      s1.set_element(i - 1, s0.element(i - 1) + interval / 2 * s0.element(i));
+    }
+
+    StateT<T, SIZE> s2{s1};
+    if (updater_) {
+      s2 = updater_(s1);
+    }
+
+    for (size_t i = SIZE - 1; i > 0; --i) {
+      s2.set_element(i - 1, s0.element(i - 1) + interval * s2.element(i));
+    }
+
+    return s2;
+  }
+};
+
+template <typename ScalarT, typename T>
+class RungeKutta2<ScalarT, T, 4> final {
+ private:
+  UpdateFn<T, 4> updater_{};
+
+ public:
+  RungeKutta2() = default;
+  RungeKutta2(UpdateFn<T, 4>&& updater) : updater_(std::forward<UpdateFn<T, 4>>(updater)) {}
+
+  StateT<T, 4> operator()(ScalarT interval, StateT<T, 4> s0) {
+    if (updater_) {
+      s0 = updater_(s0);
+    }
+    StateT<T, 4> s1{s0};
+
+    s1.template set_element<2>(s0.template element<2>() + interval / 2 * s0.template element<3>());
+    s1.template set_element<1>(s0.template element<1>() + interval / 2 * s0.template element<2>());
+    s1.template set_element<0>(s0.template element<0>() + interval / 2 * s0.template element<1>());
+
+    StateT<T, 4> s2{s1};
+    if (updater_) {
+      s2 = updater_(s1);
+    }
+
+    s2.template set_element<2>(s0.template element<2>() + interval * s2.template element<3>());
+    s2.template set_element<1>(s0.template element<1>() + interval * s2.template element<2>());
+    s2.template set_element<0>(s0.template element<0>() + interval * s2.template element<1>());
+
+    return s2;
+  }
+};
+
+template <typename ScalarT, typename T>
+class RungeKutta2<ScalarT, T, 3> final {
+ private:
+  UpdateFn<T, 3> updater_{};
+
+ public:
+  RungeKutta2() = default;
+  RungeKutta2(UpdateFn<T, 3>&& updater) : updater_(std::forward<UpdateFn<T, 3>>(updater)) {}
+
+  StateT<T, 3> operator()(ScalarT interval, StateT<T, 3> s0) {
+    if (updater_) {
+      s0 = updater_(s0);
+    }
+    StateT<T, 3> s1{s0};
+
+    s1.template set_element<1>(s0.template element<1>() + interval / 2 * s0.template element<2>());
+    s1.template set_element<0>(s0.template element<0>() + interval / 2 * s0.template element<1>());
+
+    StateT<T, 3> s2{s1};
+    if (updater_) {
+      s2 = updater_(s1);
+    }
+
+    s2.template set_element<1>(s0.template element<1>() + interval * s2.template element<2>());
+    s2.template set_element<0>(s0.template element<0>() + interval * s2.template element<1>());
+
+    return s2;
+  }
+};
+
+template <typename ScalarT, typename T>
+class RungeKutta2<ScalarT, T, 2> final {
+ private:
+  UpdateFn<T, 2> updater_{};
+
+ public:
+  RungeKutta2() = default;
+  RungeKutta2(UpdateFn<T, 2>&& updater) : updater_(std::forward<UpdateFn<T, 2>>(updater)) {}
+
+  StateT<T, 2> operator()(ScalarT interval, StateT<T, 2> s0) {
+    if (updater_) {
+      s0 = updater_(s0);
+    }
+    StateT<T, 2> s1{s0};
+
+    s1.template set_element<0>(s0.template element<0>() + interval / 2 * s0.template element<1>());
+
+    StateT<T, 2> s2{s1};
+    if (updater_) {
+      s2 = updater_(s1);
+    }
+
+    s2.template set_element<0>(s0.template element<0>() + interval * s2.template element<1>());
+
+    return s2;
   }
 };
 
--- math/integrators_test.cc
+++ math/integrators_test.cc
@@ -2,6 +2,7 @@
 
 #include "gtest/gtest.h"
 #include "math/multivector.h"
+#include "math/multivector_test_utils.h"
 #include "math/states.h"
 
 namespace ndyn::math {
@@ -298,6 +299,331 @@ TEST(ForwardEuler4StateTest, CanUpdateJerk) {
   EXPECT_EQ(expected_velocity, s1.element<1>());
   EXPECT_EQ(expected_acceleration, s1.element<2>());
   EXPECT_EQ(expected_jerk, s1.element<3>());
+}
+
+TEST(RungeKutta22StateTest, CanHandleConstantVelocity) {
+  using ScalarType = float;
+  using StateType = StateT<Vga2dMultivector<ScalarType>, 2>;
+  using ValueType = typename StateType::ValueType;
+
+  static constexpr ScalarType delta_t{0.5f};
+  static constexpr ScalarType velocity{1.f};
+  static constexpr ScalarType initial_position{3.f};
+
+  static constexpr StateType s0{{0.f, initial_position, 0.f}, {0.f, velocity, 0.f}};
+
+  static constexpr ValueType expected_position{0.f, initial_position + delta_t * velocity, 0.f};
+
+  RungeKutta2<ScalarType, ValueType, StateType::size()> stepper{};
+  StateType s1 = stepper(delta_t, s0);
+
+  EXPECT_EQ(expected_position, s1.element<0>());
+  EXPECT_EQ(s0.element<1>(), s1.element<1>());
+}
+
+TEST(RungeKutta22StateTest, CanUpdateVelocityFromPosition) {
+  using std::exp;
+
+  using ScalarType = float;
+  using StateType = StateT<Vga2dMultivector<ScalarType>, 2>;
+  using ValueType = typename StateType::ValueType;
+
+  static constexpr ScalarType delta_t{0.01f};
+  static constexpr ScalarType initial_position{3.f};
+
+  static constexpr StateType s0{{0.f, initial_position, 0.f}};
+
+  /**
+   * This test is simulating an exponential. The analytic solution is
+   *
+   * position = A * e^(-t)
+   * velocity = -A * e^(-t)
+   *
+   * where A = initial_position. We set up the expected state according to this analytic solution.
+   */
+
+  static constexpr StateType expected{{0.f, initial_position * exp(-delta_t), 0.f},  //
+                                      {0.f, -initial_position * exp(-delta_t), 0.f}};
+
+  RungeKutta2<ScalarType, ValueType, StateType::size()> stepper{
+      [](const StateType& s0) -> StateType {
+        StateType result{s0};
+        result.set_element<1>(-result.element<0>());
+        return result;
+      }};
+
+  StateType s1 = stepper(delta_t, s0);
+
+  EXPECT_TRUE(AreNear(expected.element<0>(), s1.element<0>(), 0.05f));
+  EXPECT_TRUE(AreNear(expected.element<1>(), s1.element<1>(), 0.05f));
+}
+
+TEST(RungeKutta23StateTest, CanHandleConstantVelocity) {
+  using ScalarType = float;
+  using StateType = State2d<ScalarType>;
+  using ValueType = typename StateType::ValueType;
+
+  static constexpr ScalarType delta_t{0.5f};
+  static constexpr ScalarType velocity{1.f};
+  static constexpr ScalarType initial_position{3.f};
+
+  static constexpr StateType s0{
+      {0.f, initial_position, 0.f}, {0.f, velocity, 0.f}, {0.f, 0.f, 0.f}};
+
+  static constexpr ValueType expected_position{0.f, initial_position + delta_t * velocity, 0.f};
+
+  RungeKutta2<ScalarType, ValueType, StateType::size()> stepper{};
+  StateType s1 = stepper(delta_t, s0);
+
+  EXPECT_EQ(expected_position, s1.element<0>());
+  EXPECT_EQ(s0.element<1>(), s1.element<1>());
+  EXPECT_EQ(s0.element<2>(), s1.element<2>());
+}
+
+TEST(RungeKutta23StateTest, CanHandleConstantAcceleration) {
+  using ScalarType = float;
+  using StateType = State2d<ScalarType>;
+  using ValueType = typename StateType::ValueType;
+
+  static constexpr ScalarType delta_t{0.5f};
+  static constexpr ScalarType acceleration{5.f};
+  static constexpr ScalarType initial_velocity{1.f};
+  static constexpr ScalarType initial_position{3.f};
+
+  static constexpr StateType s0{
+      {0.f, initial_position, 0.f}, {0.f, initial_velocity, 0.f}, {0.f, acceleration, 0.f}};
+
+  static constexpr ValueType expected_position{
+      0.f, initial_position + delta_t * (initial_velocity + delta_t * acceleration), 0.f};
+
+  static constexpr ValueType expected_velocity{0.f, initial_velocity + delta_t * acceleration, 0.f};
+
+  RungeKutta2<ScalarType, ValueType, StateType::size()> stepper{};
+  StateType s1 = stepper(delta_t, s0);
+
+  EXPECT_EQ(expected_position, s1.element<0>());
+  EXPECT_EQ(expected_velocity, s1.element<1>());
+  EXPECT_EQ(s0.element<2>(), s1.element<2>());
+}
+
+TEST(RungeKutta23StateTest, CanUpdateAccelerationFromPosition) {
+  using std::cos;
+  using std::sin;
+
+  using ScalarType = float;
+  using StateType = State2d<ScalarType>;
+  using ValueType = typename StateType::ValueType;
+
+  static constexpr ScalarType delta_t{0.1f};
+  static constexpr ScalarType initial_position{3.f};
+
+  static constexpr StateType s0{{0.f, initial_position, 0.f}};
+
+  /**
+   * This test is simulating an harmonic oscillator. The analytic solution is
+   *
+   * position = A * cos(t + a)
+   * velocity = -A * sin(t + a)
+   * acceleration = -A * cos(t + a)
+   *
+   * where A = initial_position, and a = 0 since we are assuming an initial velocity of zero.
+   * We set up the expected state according to this analytic solution.
+   */
+
+  static constexpr StateType expected{{0.f, initial_position * cos(delta_t), 0.f},   //
+                                      {0.f, -initial_position * sin(delta_t), 0.f},  //
+                                      {0.f, -initial_position * cos(delta_t), 0.f}};
+
+  RungeKutta2<ScalarType, ValueType, StateType::size()> stepper{
+      [](const StateType& s0) -> StateType {
+        StateType result{s0};
+        result.set_element<2>(-result.element<0>());
+        return result;
+      }};
+
+  StateType s1 = stepper(delta_t, s0);
+
+  EXPECT_TRUE(AreNear(expected.element<0>(), s1.element<0>(), 0.1f));
+  EXPECT_TRUE(AreNear(expected.element<1>(), s1.element<1>(), 0.1f));
+  EXPECT_TRUE(AreNear(expected.element<2>(), s1.element<2>(), 0.1f));
+}
+
+TEST(RungeKutta24StateTest, CanHandleConstantVelocity) {
+  using ScalarType = float;
+  using StateType = StateT<Vga2dMultivector<ScalarType>, 4>;
+  using ValueType = typename StateType::ValueType;
+
+  static constexpr ScalarType delta_t{0.5f};
+  static constexpr ScalarType velocity{1.f};
+  static constexpr ScalarType initial_position{3.f};
+
+  static constexpr StateType s0{
+      {0.f, initial_position, 0.f}, {0.f, velocity, 0.f}, {0.f, 0.f, 0.f}};
+
+  static constexpr ValueType expected_position{0.f, initial_position + delta_t * velocity, 0.f};
+
+  RungeKutta2<ScalarType, ValueType, StateType::size()> stepper{};
+  StateType s1 = stepper(delta_t, s0);
+
+  EXPECT_EQ(expected_position, s1.element<0>());
+  EXPECT_EQ(s0.element<1>(), s1.element<1>());
+  EXPECT_EQ(s0.element<2>(), s1.element<2>());
+  EXPECT_EQ(s0.element<3>(), s1.element<3>());
+}
+
+TEST(RungeKutta24StateTest, CanHandleConstantAcceleration) {
+  using ScalarType = float;
+  using StateType = StateT<Vga2dMultivector<ScalarType>, 4>;
+  using ValueType = typename StateType::ValueType;
+
+  static constexpr ScalarType delta_t{0.5f};
+  static constexpr ScalarType acceleration{5.f};
+  static constexpr ScalarType initial_velocity{1.f};
+  static constexpr ScalarType initial_position{3.f};
+
+  static constexpr StateType s0{
+      {0.f, initial_position, 0.f}, {0.f, initial_velocity, 0.f}, {0.f, acceleration, 0.f}};
+
+  static constexpr ValueType expected_position{
+      0.f, initial_position + delta_t * (initial_velocity + delta_t * acceleration), 0.f};
+
+  static constexpr ValueType expected_velocity{0.f, initial_velocity + delta_t * acceleration, 0.f};
+
+  RungeKutta2<ScalarType, ValueType, StateType::size()> stepper{};
+  StateType s1 = stepper(delta_t, s0);
+
+  EXPECT_EQ(expected_position, s1.element<0>());
+  EXPECT_EQ(expected_velocity, s1.element<1>());
+  EXPECT_EQ(s0.element<2>(), s1.element<2>());
+  EXPECT_EQ(s0.element<3>(), s1.element<3>());
+}
+
+TEST(RungeKutta24StateTest, CanUpdateAccelerationFromPosition) {
+  using std::cos;
+  using std::sin;
+
+  using ScalarType = float;
+  using StateType = StateT<Vga2dMultivector<ScalarType>, 4>;
+  using ValueType = typename StateType::ValueType;
+
+  static constexpr ScalarType delta_t{0.1f};
+  static constexpr ScalarType initial_position{3.f};
+
+  static constexpr StateType s0{{0.f, initial_position, 0.f}};
+
+  /**
+   * This test is simulating an harmonic oscillator. The analytic solution is
+   *
+   * position = A * cos(t + a)
+   * velocity = -A * sin(t + a)
+   * acceleration = -A * cos(t + a)
+   *
+   * where A = initial_position, and a = 0 since we are assuming an initial velocity of zero.
+   * We set up the expected state according to this analytic solution.
+   */
+
+  static constexpr StateType expected{{0.f, initial_position * cos(delta_t), 0.f},   //
+                                      {0.f, -initial_position * sin(delta_t), 0.f},  //
+                                      {0.f, -initial_position * cos(delta_t), 0.f}};
+
+  RungeKutta2<ScalarType, ValueType, StateType::size()> stepper{
+      [](const StateType& s0) -> StateType {
+        StateType result{s0};
+        result.set_element<2>(-result.element<0>());
+        return result;
+      }};
+
+  StateType s1 = stepper(delta_t, s0);
+
+  LOG(INFO) << "s1: " << s1;
+
+  EXPECT_TRUE(AreNear(expected.element<0>(), s1.element<0>(), 0.1f));
+  EXPECT_TRUE(AreNear(expected.element<1>(), s1.element<1>(), 0.1f));
+  EXPECT_TRUE(AreNear(expected.element<2>(), s1.element<2>(), 0.1f));
+  EXPECT_TRUE(AreNear(expected.element<3>(), s1.element<3>(), 0.1f));
+}
+
+TEST(RungeKutta24StateTest, CanHandleConstantJerk) {
+  using ScalarType = float;
+  using StateType = StateT<Vga2dMultivector<ScalarType>, 4>;
+  using ValueType = typename StateType::ValueType;
+
+  static constexpr ScalarType delta_t{0.5f};
+  static constexpr ScalarType jerk{7.f};
+  static constexpr ScalarType initial_acceleration{5.f};
+  static constexpr ScalarType initial_velocity{1.f};
+  static constexpr ScalarType initial_position{3.f};
+
+  static constexpr StateType s0{{0.f, initial_position, 0.f},
+                                {0.f, initial_velocity, 0.f},
+                                {0.f, initial_acceleration, 0.f},
+                                {0.f, jerk, 0.f}};
+
+  static constexpr ValueType expected_position{
+      0.f,
+      initial_position +
+          delta_t * (initial_velocity + delta_t * (initial_acceleration + delta_t * jerk)),
+      0.f};
+
+  static constexpr ValueType expected_velocity{
+      0.f, initial_velocity + delta_t * (initial_acceleration + delta_t * jerk), 0.f};
+  static constexpr ValueType expected_acceleration{0.f, initial_acceleration + delta_t * jerk, 0.f};
+
+  RungeKutta2<ScalarType, ValueType, StateType::size()> stepper{};
+  StateType s1 = stepper(delta_t, s0);
+
+  EXPECT_EQ(expected_position, s1.element<0>());
+  EXPECT_EQ(expected_velocity, s1.element<1>());
+  EXPECT_EQ(expected_acceleration, s1.element<2>());
+  EXPECT_EQ(s0.element<3>(), s1.element<3>());
+}
+
+TEST(RungeKutta24StateTest, CanUpdateJerk) {
+  using std::cos;
+  using std::sin;
+
+  using ScalarType = float;
+  using StateType = StateT<Vga2dMultivector<ScalarType>, 4>;
+  using ValueType = typename StateType::ValueType;
+
+  static constexpr ScalarType delta_t{0.05f};
+  static constexpr ScalarType initial_position{3.f};
+
+  static constexpr StateType s0{{0.f, 0.f, 0.f}, {0.f, initial_position, 0.f}};
+
+  /**
+   * This test is simulating an harmonic oscillator but with an odometer that takes off distance
+   * when put in reverse. (See _Ferris Bueller's Day Off_ for details of this odometer design.) The
+   * test name includes the concept of jerk, since we are dealing with a 3rd derivative in some
+   * sense.
+   *
+   * odometer = A * sin(t + a)
+   * position = A * cos(t + a)
+   * velocity = -A * sin(t + a)
+   * acceleration = -A * cos(t + a)
+   *
+   * where A = initial_position, and a = 0 since we are assuming an initial velocity of zero.
+   * We set up the expected state according to this analytic solution.
+   */
+
+  static constexpr StateType expected{{0.f, initial_position * sin(delta_t), 0.f},   //
+                                      {0.f, initial_position * cos(delta_t), 0.f},   //
+                                      {0.f, -initial_position * sin(delta_t), 0.f},  //
+                                      {0.f, -initial_position * cos(delta_t), 0.f}};
+
+  RungeKutta2<ScalarType, ValueType, StateType::size()> stepper{
+      [](const StateType& s0) -> StateType {
+        StateType result{s0};
+        result.set_element<3>(-result.element<1>());
+        return result;
+      }};
+
+  StateType s1 = stepper(delta_t, s0);
+
+  EXPECT_TRUE(AreNear(expected.element<0>(), s1.element<0>(), 0.01f));
+  EXPECT_TRUE(AreNear(expected.element<1>(), s1.element<1>(), 0.01f));
+  EXPECT_TRUE(AreNear(expected.element<2>(), s1.element<2>(), 0.01f));
+  EXPECT_TRUE(AreNear(expected.element<3>(), s1.element<3>(), 0.01f));
 }
 
 }  // namespace ndyn::math
