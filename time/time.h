b6044a840c94f783b558f25d4d4bc654b90515fd
James Pack <jgrimm@gmail.com>
2024-09-12 09:45:39 -0400

    Trivial setup of time as a scalar type

---
+++ time/time.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <cstring>
+
+namespace ndyn::time {
+
+// Type for representing times (usually a count of seconds, milliseconds, or microseconds) as
+// scalars.
+using TimeT = uint64_t;
+
+}  // namespace ndyn::time
