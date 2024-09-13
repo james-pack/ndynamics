dec80620dc60a914f3bdc53fc471613e5f08f3ae
James Pack <jgrimm@gmail.com>
2024-09-13 08:40:11 -0400

    Fix bug where the MeasurementChannel was being copy constructed, instead of referenced. Deleted copy constructors from the MeasurementChannel class to prevent this in the future.

--- sensor/imu_bmi323_test.cc
+++ sensor/imu_bmi323_test.cc
@@ -15,7 +15,7 @@ TEST(ImuBmi323Test, CanCompile) {
   Bus<BusType::FAKE_BUS, uint8_t, uint8_t> bus{};
   Sensor<SensorSku::IMU_BMI323, BusType::FAKE_BUS> bmi323{
       bus, ImuBmi323_AllowedI2CAddress::LOWER_ADDRESS};
-  TemperatureMeasurementChannel temperature{bmi323.temperature_measurements()};
+  const TemperatureMeasurementChannel& temperature{bmi323.temperature_measurements()};
 
   EXPECT_FALSE(temperature.has_value_available());
 }
--- sensor/measurement_channel.h
+++ sensor/measurement_channel.h
@@ -2,6 +2,7 @@
 
 #include <utility>
 
+#include "glog/logging.h"
 #include "sensor/measurement_type.h"
 #include "time/time.h"
 
@@ -18,9 +19,20 @@ class MeasurementChannel final {
   mutable bool is_ready_{};
 
  public:
+  constexpr MeasurementChannel() = default;
+  constexpr MeasurementChannel(MeasurementChannel&&) = default;
+  constexpr MeasurementChannel& operator=(MeasurementChannel&&) = default;
+
+  constexpr MeasurementChannel(const MeasurementChannel&) = delete;
+  constexpr MeasurementChannel& operator=(const MeasurementChannel&) = delete;
+
   static constexpr MeasurementType type() { return TYPE; }
 
-  constexpr bool has_value_available() const { return is_ready_; }
+  constexpr bool has_value_available() const {
+    LOG(INFO) << "MeasurementChannel::has_value_available() -- is_ready_: " << is_ready_
+              << ", time_: " << time_ << ", value_: " << value_;
+    return is_ready_;
+  }
 
   constexpr time::TimeT time() const { return time_; }
 
@@ -34,10 +46,13 @@ class MeasurementChannel final {
     return {time_, value_};
   }
 
-  constexpr void set_value(time::TimeT time, ValueType value) {
-    time_ = time;
+  constexpr void set_value(time::TimeT t, ValueType value) {
+    LOG(INFO) << "MeasurementChannel::set_value() -- t: " << t << ", value: " << value;
+    time_ = t;
     value_ = value;
     is_ready_ = true;
+    LOG(INFO) << "MeasurementChannel::set_value() -- is_ready_: " << is_ready_
+              << ", time_: " << time_ << ", value_: " << value_;
   }
 };
 
