241c587a20dc7ca10a1e7bc451982a650959fa7f
James Pack <jgrimm@gmail.com>
2024-09-17 13:11:55 -0400

    Added ability to inject noise onto non-scalar measurements

--- simulation/characterization.h
+++ simulation/characterization.h
@@ -14,7 +14,7 @@ namespace ndyn::simulation {
 static constexpr sensor::MeasurementValueType<sensor::MeasurementType::TEMPERATURE>::type
     DEFAULT_CHARACTERIZATION_TEMPERATURE{25};
 
-template <typename ValueType>
+template <typename ScalarType>
 class Characteristic final {
  public:
   using TemperatureType = sensor::MeasurementValueType<sensor::MeasurementType::TEMPERATURE>::type;
@@ -23,49 +23,49 @@ class Characteristic final {
   // where the measurement capability was specified or measured.
   TemperatureType temperature{DEFAULT_CHARACTERIZATION_TEMPERATURE};
 
-  ValueType offset_average;
-  ValueType offset_std;
+  ScalarType offset_average;
+  ScalarType offset_std;
 };
 
-template <typename ValueType>
+template <typename ValueType, typename ScalarType = ValueType>
 class Characterization final {
  public:
   using TemperatureType = sensor::MeasurementValueType<sensor::MeasurementType::TEMPERATURE>::type;
 
  private:
   std::mt19937 random_generator_{};
-  std::vector<Characteristic<ValueType>> characteristics_{};
+  std::vector<Characteristic<ScalarType>> characteristics_{};
 
  public:
-  constexpr Characterization(std::initializer_list<Characteristic<ValueType>> characteristics)
+  constexpr Characterization(std::initializer_list<Characteristic<ScalarType>> characteristics)
       : characteristics_(characteristics) {
     if (characteristics_.empty()) {
       throw std::domain_error("Cannot create a characterization without any characteristics");
     }
     std::sort(
         characteristics_.begin(), characteristics_.end(),
-        [](const Characteristic<ValueType>& lhs, const Characteristic<ValueType>& rhs) -> bool {
+        [](const Characteristic<ScalarType>& lhs, const Characteristic<ScalarType>& rhs) -> bool {
           return lhs.temperature < rhs.temperature;
         });
   }
 
   // Allow for explicit seed value for repeatable simulations and testing.
   constexpr Characterization(uint32_t seed,
-                             std::initializer_list<Characteristic<ValueType>> characteristics)
+                             std::initializer_list<Characteristic<ScalarType>> characteristics)
       : random_generator_(seed), characteristics_(characteristics) {
     if (characteristics_.empty()) {
       throw std::domain_error("Cannot create a characterization without any characteristics");
     }
     std::sort(
         characteristics_.begin(), characteristics_.end(),
-        [](const Characteristic<ValueType>& lhs, const Characteristic<ValueType>& rhs) -> bool {
+        [](const Characteristic<ScalarType>& lhs, const Characteristic<ScalarType>& rhs) -> bool {
           return lhs.temperature < rhs.temperature;
         });
   }
 
   ValueType inject_noise(TemperatureType temperature, ValueType value) {
-    const Characteristic<ValueType>* lower{nullptr};
-    const Characteristic<ValueType>* upper{nullptr};
+    const Characteristic<ScalarType>* lower{nullptr};
+    const Characteristic<ScalarType>* upper{nullptr};
     for (const auto& c : characteristics_) {
       if (c.temperature <= temperature &&
           (lower == nullptr || c.temperature > lower->temperature)) {
@@ -76,27 +76,27 @@ class Characterization final {
       }
     }
 
-    ValueType noise_factor{};
+    ScalarType noise_factor{};
     if (lower != nullptr && upper != nullptr) {
       // Interpolate between the two values.
       TemperatureType full_distance{upper->temperature - lower->temperature};
       TemperatureType distance_to_lower{temperature - lower->temperature};
       TemperatureType distance_to_upper{upper->temperature - temperature};
 
-      const ValueType average{
+      const ScalarType average{
           (distance_to_upper * lower->offset_average + distance_to_lower * upper->offset_average) /
           full_distance};
-      const ValueType std{
+      const ScalarType std{
           (distance_to_upper * lower->offset_std + distance_to_lower * upper->offset_std) /
           full_distance};
 
-      std::normal_distribution<ValueType> distribution{average, std};
+      std::normal_distribution<ScalarType> distribution{average, std};
       noise_factor = distribution(random_generator_);
     } else if (lower != nullptr) {
-      std::normal_distribution<ValueType> distribution{lower->offset_average, lower->offset_std};
+      std::normal_distribution<ScalarType> distribution{lower->offset_average, lower->offset_std};
       noise_factor = distribution(random_generator_);
     } else if (upper != nullptr) {
-      std::normal_distribution<ValueType> distribution{upper->offset_average, upper->offset_std};
+      std::normal_distribution<ScalarType> distribution{upper->offset_average, upper->offset_std};
       noise_factor = distribution(random_generator_);
     } else {
       throw std::runtime_error(
