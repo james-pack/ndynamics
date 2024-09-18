4d7c36454edfc2e22d7d9c0299e2e4b576a9cb44
James Pack <jgrimm@gmail.com>
2024-09-17 22:00:45 -0400

    Extracted concept of a DataSeries, rather than explicit array manipulations.

--- simulation/pendulum_graph.cc
+++ simulation/pendulum_graph.cc
@@ -15,6 +15,33 @@
 
 namespace ndyn::simulation {
 
+template <typename T, size_t NUM_POINTS, size_t NUM_DIMENSIONS = 1>
+class DataSeries final {
+ private:
+  std::array<std::array<T, NUM_POINTS>, NUM_DIMENSIONS + 1> data_{};
+
+ public:
+  void update(T x, const std::array<T, NUM_DIMENSIONS>& y) {
+    using std::copy_n;
+    for (size_t i = 0; i < NUM_DIMENSIONS + 1; ++i) {
+      copy_n(data_[i].begin() + 1, data_[i].size() - 1, data_[i].begin());
+    }
+    data_[0][NUM_POINTS - 1] = x;
+    for (size_t i = 0; i < NUM_DIMENSIONS; ++i) {
+      data_.at(i + 1).at(NUM_POINTS - 1) = y.at(i);
+    }
+  }
+
+  const T* x_data() const { return data_[0].data(); }
+
+  template <size_t DIMENSION = 0>
+  const T* y_data() const {
+    return data_.at(DIMENSION + 1).data();
+  }
+
+  size_t size() const { return NUM_POINTS; }
+};
+
 class PendulumGraph : public ui::App {
  private:
   using AccelerometerTypes = sensor::MeasurementValueType<sensor::MeasurementType::ACCELEROMETER>;
@@ -39,131 +66,96 @@ class PendulumGraph : public ui::App {
 
   // Show 7 full periods at a time with 128 points per period.
   static constexpr size_t NUM_POINTS{
-      static_cast<size_t>(128 * 4 * compute_period(LENGTH, GRAVITY_ACCELERATION, ANGLE))};
+      static_cast<size_t>(64 * 4 * compute_period(LENGTH, GRAVITY_ACCELERATION, ANGLE))};
 
-  std::array<FloatT, NUM_POINTS> x1{};
-  std::array<FloatT, NUM_POINTS> y1{};
-
-  std::array<FloatT, NUM_POINTS> x2{};
-  std::array<FloatT, NUM_POINTS> y2{};
-
-  std::array<FloatT, NUM_POINTS> x3{};
-  std::array<FloatT, NUM_POINTS> y3{};
-
-  std::array<FloatT, NUM_POINTS> x4{};
-  std::array<FloatT, NUM_POINTS> y4{};
-
-  std::array<FloatT, NUM_POINTS> x5{};
-  std::array<FloatT, NUM_POINTS> y5{};
-
-  std::array<FloatT, NUM_POINTS> x6{};
-  std::array<FloatT, NUM_POINTS> y6{};
-
-  std::array<FloatT, NUM_POINTS> x7{};
-  std::array<FloatT, NUM_POINTS> y7{};
-
-  std::array<FloatT, NUM_POINTS> x8{};
-  std::array<FloatT, NUM_POINTS> y8{};
+  DataSeries<FloatT, NUM_POINTS, 4> position_series{};
+  DataSeries<FloatT, NUM_POINTS, 3> acceleration_series{};
+  DataSeries<FloatT, NUM_POINTS, 1> theta_series{};
+  DataSeries<FloatT, NUM_POINTS, 3> energy_series{};
 
   FloatT previous_time{};
 
  protected:
   void Update() override {
-    using std::copy_n;
     using std::exp;
     using std::sin;
 
     // Every 5 seconds, we display cycle through one period of the pendulum.
     const FloatT current_time{static_cast<FloatT>(ImGui::GetTime()) * pendulum.period() / 5};
 
-    copy_n(x1.begin() + 1, x1.size(), x1.begin());
-    copy_n(y1.begin() + 1, y1.size(), y1.begin());
-    copy_n(x2.begin() + 1, x2.size(), x2.begin());
-    copy_n(y2.begin() + 1, y2.size(), y2.begin());
-
-    copy_n(x3.begin() + 1, x3.size(), x3.begin());
-    copy_n(y3.begin() + 1, y3.size(), y3.begin());
-
-    copy_n(x4.begin() + 1, x4.size(), x4.begin());
-    copy_n(y4.begin() + 1, y4.size(), y4.begin());
-
-    copy_n(x5.begin() + 1, x5.size(), x5.begin());
-    copy_n(y5.begin() + 1, y5.size(), y5.begin());
-
-    copy_n(x6.begin() + 1, x6.size(), x6.begin());
-    copy_n(y6.begin() + 1, y6.size(), y6.begin());
-
-    copy_n(x7.begin() + 1, x7.size(), x7.begin());
-    copy_n(y7.begin() + 1, y7.size(), y7.begin());
-
-    copy_n(x8.begin() + 1, x8.size(), x8.begin());
-    copy_n(y8.begin() + 1, y8.size(), y8.begin());
-
     pendulum.goto_time(current_time);
-
-    x1[x1.size() - 1] = current_time;
-    y1[x1.size() - 1] = pendulum.height();
-
-    x2[x2.size() - 1] = current_time;
-    y2[x2.size() - 1] = pendulum.compute_potential_energy();
-
-    x3[x3.size() - 1] = current_time;
-    y3[x3.size() - 1] = pendulum.compute_kinetic_energy();
-
-    x4[x4.size() - 1] = current_time;
-    y4[x4.size() - 1] = pendulum.compute_kinetic_energy() + pendulum.compute_potential_energy();
-
-    const auto theta{pendulum.theta()};
-    x5[x5.size() - 1] = current_time;
-    y5[x5.size() - 1] = theta;
-
-    const auto& acceleration{pendulum.acceleration()};
-    const auto fuzzed_acceleration{characterization.inject_noise(TEMPERATURE, acceleration)};
-    x6[x6.size() - 1] = current_time;
-    y6[x6.size() - 1] = fuzzed_acceleration.component(1);
-    x7[x7.size() - 1] = current_time;
-    y7[x7.size() - 1] = fuzzed_acceleration.component(2);
-    x8[x8.size() - 1] = current_time;
-    y8[x8.size() - 1] = fuzzed_acceleration.component(4);
-
-    VLOG(2) << "current_time: " << current_time << ", theta: " << y5[x5.size() - 1]
-            << ", height: " << y1[x1.size() - 1];
-
     previous_time = current_time;
+
+    {
+      const auto position{pendulum.position()};
+      position_series.update(current_time, {position.component(1), position.component(2),
+                                            position.component(4), pendulum.height()});
+    }
+
+    theta_series.update(current_time, {pendulum.theta()});
+
+    {
+      const auto kinetic_energy{pendulum.compute_kinetic_energy()};
+      const auto potential_energy{pendulum.compute_potential_energy()};
+      const auto total_energy{kinetic_energy + potential_energy};
+      energy_series.update(current_time, {kinetic_energy, potential_energy, total_energy});
+    }
+
+    {
+      const auto acceleration{pendulum.acceleration()};
+      const auto fuzzed_acceleration{characterization.inject_noise(TEMPERATURE, acceleration)};
+      acceleration_series.update(
+          current_time,
+          {acceleration.component(1), acceleration.component(2), acceleration.component(4)});
+    }
 
     auto size{ImGui::GetContentRegionAvail()};
     size.y /= 4;
 
     if (ImPlot::BeginPlot("Position", size)) {
-      ImPlot::SetupAxes("t", "h", ImPlotAxisFlags_AutoFit, ImPlotAxisFlags_AutoFit);
+      ImPlot::SetupAxes("t", "position", ImPlotAxisFlags_AutoFit, ImPlotAxisFlags_AutoFit);
       ImPlot::SetNextMarkerStyle(ImPlotMarker_Circle);
-      ImPlot::PlotScatter("Height", x1.data(), y1.data(), x1.size());
+      // ImPlot::PlotScatter("x", position_series.x_data(), position_series.y_data<0>(),
+      //                     position_series.size());
+      // ImPlot::PlotScatter("y", position_series.x_data(), position_series.y_data<1>(),
+      //                     position_series.size());
+      // ImPlot::PlotScatter("z", position_series.x_data(), position_series.y_data<2>(),
+      //                     position_series.size());
+      ImPlot::PlotScatter("height", position_series.x_data(), position_series.y_data<3>(),
+                          position_series.size());
       ImPlot::EndPlot();
     }
 
     if (ImPlot::BeginPlot("Energy", size)) {
       ImPlot::SetupAxes("t", "Energy", ImPlotAxisFlags_AutoFit, ImPlotAxisFlags_AutoFit);
       ImPlot::SetNextMarkerStyle(ImPlotMarker_Circle);
-      ImPlot::PlotScatter("Potential", x2.data(), y2.data(), x2.size());
+      ImPlot::PlotScatter("Kinetic", energy_series.x_data(), energy_series.y_data<0>(),
+                          energy_series.size());
+      ImPlot::PlotScatter("Potential", energy_series.x_data(), energy_series.y_data<1>(),
+                          energy_series.size());
       ImPlot::SetNextMarkerStyle(ImPlotMarker_Cross);
-      ImPlot::PlotScatter("Kinetic", x3.data(), y3.data(), x3.size());
-      ImPlot::PlotScatter("Total", x4.data(), y4.data(), x4.size());
+      ImPlot::PlotScatter("Total", energy_series.x_data(), energy_series.y_data<2>(),
+                          energy_series.size());
       ImPlot::EndPlot();
     }
 
     if (ImPlot::BeginPlot("Angle", size)) {
       ImPlot::SetupAxes("t", "Theta", ImPlotAxisFlags_AutoFit, ImPlotAxisFlags_AutoFit);
       ImPlot::SetNextMarkerStyle(ImPlotMarker_Circle);
-      ImPlot::PlotScatter("theta", x5.data(), y5.data(), x5.size());
+      ImPlot::PlotScatter("theta", theta_series.x_data(), theta_series.y_data(),
+                          theta_series.size());
       ImPlot::EndPlot();
     }
 
     if (ImPlot::BeginPlot("Accelerometer Measurements", size)) {
       ImPlot::SetupAxes("t", "Acceleration", ImPlotAxisFlags_AutoFit, ImPlotAxisFlags_AutoFit);
       ImPlot::SetNextMarkerStyle(ImPlotMarker_Circle);
-      ImPlot::PlotScatter("x", x6.data(), y6.data(), x6.size());
-      ImPlot::PlotScatter("y", x7.data(), y7.data(), x7.size());
-      ImPlot::PlotScatter("z", x8.data(), y8.data(), x8.size());
+      ImPlot::PlotScatter("x", acceleration_series.x_data(), acceleration_series.y_data<0>(),
+                          acceleration_series.size());
+      ImPlot::PlotScatter("y", acceleration_series.x_data(), acceleration_series.y_data<1>(),
+                          acceleration_series.size());
+      ImPlot::PlotScatter("z", acceleration_series.x_data(), acceleration_series.y_data<2>(),
+                          acceleration_series.size());
       ImPlot::EndPlot();
     }
   }
