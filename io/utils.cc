c97e413ce8a3917b1cc861f39c6410bc0cbf697d
James Pack <jgrimm@gmail.com>
2024-09-22 17:02:56 -0400

    Fixed bug in read_file() function.

--- io/utils.cc
+++ io/utils.cc
@@ -8,6 +8,7 @@ namespace ndyn::io {
 
 std::string read_file(std::filesystem::path path) {
   std::string contents{};
+
   static constexpr size_t BUFFER_SIZE{1024};
   char buffer[BUFFER_SIZE];
   std::ifstream file{path, std::ios::binary | std::ios_base::in};
@@ -16,6 +17,10 @@ std::string read_file(std::filesystem::path path) {
   while (bytes_read == BUFFER_SIZE) {
     file.read(buffer, BUFFER_SIZE);
     bytes_read = file.gcount();
+    if (bytes_read < BUFFER_SIZE) {
+      // The file.read() call doesn't null-terminate its output in binary mode.
+      buffer[bytes_read] = '\0';
+    }
     contents.append(buffer);
   }
 
