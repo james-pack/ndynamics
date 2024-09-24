60f4d181a5d05a0795e43c41a74e822aa5df0bd3
James Pack <jgrimm@gmail.com>
2024-09-24 05:03:01 -0400

    Centralized loading and compiling of shaders into a single method.

--- ui/shader_program.cc
+++ ui/shader_program.cc
@@ -13,72 +13,48 @@
 
 namespace ndyn::ui {
 
-GLuint initialize_vertex_shader(std::filesystem::path vertex_file_path) {
-  GLuint vertex_shader_id = glCreateShader(GL_VERTEX_SHADER);
+template <GLenum SHADER_TYPE>
+GLuint initialize_shader(std::filesystem::path path) {
+  GLuint shader_id = glCreateShader(SHADER_TYPE);
 
   GLint result{GL_FALSE};
   int info_log_length{0};
 
-  const std::string vertex_shader_code{io::read_file(vertex_file_path)};
-  VLOG(2) << "vertex shader:\n" << vertex_shader_code << "\n";
-  const char *vertex_source_pointer = vertex_shader_code.c_str();
-  glShaderSource(vertex_shader_id, 1, &vertex_source_pointer, NULL);
-  glCompileShader(vertex_shader_id);
+  const std::string shader_code{io::read_file(path)};
+  VLOG(2) << "shader source:\n" << shader_code << "\n";
+  const char *source_pointer = shader_code.c_str();
+  glShaderSource(shader_id, 1, &source_pointer, nullptr);
+  glCompileShader(shader_id);
 
-  glGetShaderiv(vertex_shader_id, GL_COMPILE_STATUS, &result);
+  glGetShaderiv(shader_id, GL_COMPILE_STATUS, &result);
   if (result == GL_FALSE) {
-    glGetShaderiv(vertex_shader_id, GL_INFO_LOG_LENGTH, &info_log_length);
+    glGetShaderiv(shader_id, GL_INFO_LOG_LENGTH, &info_log_length);
     if (info_log_length > 0) {
       LOG(INFO) << "result: " << result << ", info_log_length: " << info_log_length;
       std::vector<GLchar> error_message(info_log_length);
-      glGetShaderInfoLog(vertex_shader_id, info_log_length, &info_log_length, &error_message[0]);
-      LOG(FATAL) << "Could not compile vertex shader: " << &error_message[0];
+      glGetShaderInfoLog(shader_id, info_log_length, &info_log_length, &error_message[0]);
+      LOG(FATAL) << "Could not compile shader '" << path << "'. Error: " << &error_message[0];
     }
   }
 
-  return vertex_shader_id;
-}
-
-GLuint initialize_fragment_shader(std::filesystem::path fragment_file_path) {
-  GLuint fragment_shader_id = glCreateShader(GL_FRAGMENT_SHADER);
-
-  GLint result{GL_FALSE};
-  int info_log_length{0};
-
-  const std::string fragment_shader_code{io::read_file(fragment_file_path)};
-  VLOG(2) << "fragment shader:\n" << fragment_shader_code << "\n";
-  const char *fragment_source_pointer = fragment_shader_code.c_str();
-  glShaderSource(fragment_shader_id, 1, &fragment_source_pointer, NULL);
-  glCompileShader(fragment_shader_id);
-
-  glGetShaderiv(fragment_shader_id, GL_COMPILE_STATUS, &result);
-  if (result == GL_FALSE) {
-    glGetShaderiv(fragment_shader_id, GL_INFO_LOG_LENGTH, &info_log_length);
-    if (info_log_length > 0) {
-      LOG(INFO) << "result: " << result << ", info_log_length: " << info_log_length;
-      std::vector<GLchar> error_message(info_log_length);
-      glGetShaderInfoLog(fragment_shader_id, info_log_length, &info_log_length, &error_message[0]);
-      LOG(FATAL) << "Could not compile fragment shader: " << &error_message[0];
-    }
-  }
-
-  return fragment_shader_id;
+  return shader_id;
 }
 
 ShaderProgram ShaderProgramBuilder::build() {
   std::vector<GLuint> shader_ids{};
   shader_ids.reserve(vertex_shader_paths_.size() + fragment_shader_paths_.size());
 
+  // Initialize each of the vertex shaders, including compiling them.
   for (const auto path : vertex_shader_paths_) {
-    shader_ids.push_back(initialize_vertex_shader(path));
+    shader_ids.push_back(initialize_shader<GL_VERTEX_SHADER>(path));
   }
 
+  // Same for fragment shaders.
   for (const auto path : fragment_shader_paths_) {
-    shader_ids.push_back(initialize_fragment_shader(path));
+    shader_ids.push_back(initialize_shader<GL_FRAGMENT_SHADER>(path));
   }
 
   // Link the program
-  LOG(INFO) << "Linking shader program.";
   GLuint program_id = glCreateProgram();
 
   for (const auto shader_id : shader_ids) {
