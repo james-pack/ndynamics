85f457dda71cbc69010b30fbbfa4b4220a19aab5
James Pack <jgrimm@gmail.com>
2024-09-24 05:04:49 -0400

    Update the layout of the children of the Window UI element.

--- ui/ui_elements.cc
+++ ui/ui_elements.cc
@@ -4,26 +4,39 @@
 
 #include "ui/ui_elements.h"
 
+#include "glog/logging.h"
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
+#include "ui/imgui_utils.h"
 
 namespace ndyn::ui {
 
 void Window::update() {
-  static constexpr float PAD{10};
   const ImGuiViewport* viewport = ImGui::GetMainViewport();
 
   ImVec2 work_pos = viewport->WorkPos;  // Use work area to avoid menu-bar/task-bar, if any!
   ImVec2 work_size = viewport->WorkSize;
-  ImVec2 window_pos, window_pos_pivot;
-  window_pos.x = work_pos.x + work_size.x - PAD;
-  window_pos.y = work_pos.y + PAD;
+
+  ImVec2 window_size, window_pos, window_pos_pivot;
+  window_size.x = work_size.x * 0.35;  // Use a fraction of the available horizontal space.
+  window_size.y = 0.f;                 // Auto-fit the y-axis.
+
+  // We are going to right-justify this next window.
+  window_pos.x = work_pos.x + work_size.x;
+  window_pos.y = work_pos.y;
+
+  // Use the top-right of the next window as the point to position the window.
   window_pos_pivot.x = 1.0f;
   window_pos_pivot.y = 0.0f;
-  ImGui::SetNextWindowPos(window_pos, ImGuiCond_Always, window_pos_pivot);
 
-  ImGui::SetNextWindowBgAlpha(0.35f);  // Transparent background
+  VLOG(6) << "work_pos: " << work_pos << ", work_size: " << work_size
+          << ", window_pos: " << window_pos << ", window_pos_pivot: " << window_pos_pivot;
+
+  ImGui::SetNextWindowPos(window_pos, ImGuiCond_Always, window_pos_pivot);
+  ImGui::SetNextWindowSize(window_size, ImGuiCond_Always);
+
+  ImGui::SetNextWindowBgAlpha(0.1f);  // Transparent background
 
   const ImGuiWindowFlags window_flags{
       ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_AlwaysAutoResize |
