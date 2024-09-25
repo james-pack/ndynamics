62645e5da9fc38f185b4cd76562bae7d26bd7439
James Pack <jgrimm@gmail.com>
2024-09-25 04:30:15 -0400

    Add utility to generate a string representation of an ImGui key chord. Relies on English-only debug method in ImGui.

--- ui/imgui_utils.cc
+++ ui/imgui_utils.cc
@@ -16,3 +16,34 @@ std::ostream &operator<<(std::ostream &os, const ImVec2 &v) {
   os << to_string(v);
   return os;
 }
+
+namespace ndyn::ui {
+
+std::string key_chord_to_string(ImGuiKeyChord chord) {
+  std::string result{};
+
+  if (chord & ImGuiMod_Ctrl) {
+    result.append("Ctrl+");
+  }
+  if (chord & ImGuiMod_Alt) {
+    result.append("Alt+");
+  }
+  if (chord & ImGuiMod_Shift) {
+    result.append("Shift+");
+  }
+  if (chord & ImGuiMod_Super) {
+    result.append("Super+");
+  }
+
+  const ImGuiKey key_code{chord & ~ImGuiMod_Mask_};
+  const auto *name{ImGui::GetKeyName(key_code)};
+  if (name && name[0] != '\0') {
+    result.append(name);
+  } else {
+    result.append("(unknown key)");
+  }
+
+  return result;
+}
+
+}  // namespace ndyn::ui
--- ui/imgui_utils.h
+++ ui/imgui_utils.h
@@ -7,3 +7,9 @@
 
 std::string to_string(const ImVec2&);
 std::ostream& operator<<(std::ostream&, const ImVec2&);
+
+namespace ndyn::ui {
+
+std::string key_chord_to_string(ImGuiKeyChord chord);
+
+}  // namespace ndyn::ui
