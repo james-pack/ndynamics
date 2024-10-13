51e804e7351db165ed1948dea9d88cc2fc681413
James Pack <jgrimm@gmail.com>
2024-10-13 12:32:09 -0400

    Moved vector test utilities to the general :testing target in the math package.

--- math/vector_tests.h
+++ math/vector_tests.h
@@ -8,41 +8,10 @@
 #include "math/coordinates.h"
 #include "math/unit_set.h"
 #include "math/vector.h"
+#include "math/vector_test_utils.h"
 #include "units.h"
 
 namespace ndyn::math {
-
-template <typename ScalarType>
-ScalarType epsilon(ScalarType scale, ScalarType absolute = 0.0001) {
-  using std::abs;
-  return std::max(abs(scale * static_cast<ScalarType>(0.00001)), abs(absolute));
-}
-
-template <typename VectorType>
-::testing::AssertionResult AreNear(const VectorType& lhs, const VectorType& rhs,
-                                   typename VectorType::ScalarType epsilon) {
-  using std::abs;
-  using std::to_string;
-  const auto difference{lhs - rhs};
-  epsilon = abs(epsilon);
-  for (size_t i = 0; i < VectorType::size(); ++i) {
-    if (abs(difference.element(i)) > epsilon) {
-      return ::testing::AssertionFailure()
-             << "lhs: " << lhs << ", rhs: " << rhs << ", difference: " << difference
-             << " (epsilon: " << to_string(epsilon) << ")";
-    }
-  }
-  return ::testing::AssertionSuccess()
-         << "lhs: " << lhs << ", rhs: " << rhs << ", difference: " << difference
-         << " (epsilon: " << to_string(epsilon) << ")";
-}
-
-template <typename VectorType>
-::testing::AssertionResult AreNear(const VectorType& lhs, const VectorType& rhs) {
-  using ScalarType = typename VectorType::ScalarType;
-  const ScalarType greater_mag{std::max(lhs.square_magnitude(), rhs.square_magnitude())};
-  return AreNear(lhs, rhs, epsilon(greater_mag));
-}
 
 template <Coordinates COORD, size_t DIM, typename ScalarT = float>
 class VectorTest : public ::testing::Test {
