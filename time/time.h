8e8cfe1edd1d6cee6ba2446e81f157b1c96e3cfb
James Pack <jgrimm@gmail.com>
2024-10-18 10:37:55 -0400

    Changed time type name

--- time/time.h
+++ time/time.h
@@ -6,6 +6,8 @@ namespace ndyn::time {
 
 // Type for representing times (usually a count of seconds, milliseconds, or microseconds) as
 // scalars.
-using TimeT = uint64_t;
+using TimeType = uint64_t;
+// Retained for backward compatibility.
+using TimeT = TimeType;
 
 }  // namespace ndyn::time
