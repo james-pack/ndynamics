1771f0f9de65df24a0044710e087cac5939dd60a
James Pack <jgrimm@gmail.com>
2024-10-20 23:03:21 -0400

    Checkpointing additional work on the implementation for a generic element being rendered on a GPU

--- graphics/gpu_element.h
+++ graphics/gpu_element.h
@@ -6,7 +6,6 @@
 #include "glm/glm.hpp"
 #include "graphics/shader_program.h"
 #include "math/geometry.h"
-#include "time/time.h"
 
 namespace ndyn::graphics {
 
@@ -22,15 +21,23 @@ class GpuElement {
 
   void update(ScalarType t, const ShaderProgram& program,
               const math::Transform<ScalarType>& transform) {
+    // The transform argument in the local coordinates of this element.
+    const math::Transform<ScalarType> local_transform{compose_transform(t, transform)};
+
     glUseProgram(program.id());
     const GLuint element_matrix_id{glGetUniformLocation(program.id(), "element_matrix")};
-    const glm::mat4 element_matrix{transform_to_glm_mat4(transform)};
+    const glm::mat4 element_matrix{transform_to_glm_mat4(local_transform)};
     glUniformMatrix4fv(element_matrix_id, 1, GL_FALSE, &element_matrix[0][0]);
 
     draw(t);
   }
 
-  virtual void draw(ScalarType t) = 0;
+  virtual math::Transform<ScalarType> compose_transform(
+      ScalarType /*time*/, const math::Transform<ScalarType>& transform) {
+    return transform;
+  }
+
+  virtual void draw(ScalarType t) {}
 };
 
 }  // namespace ndyn::graphics
