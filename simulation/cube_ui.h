e886fcc75152583e37d9e11a787d3e06ac8301ed
James Pack <jgrimm@gmail.com>
2024-11-13 15:11:14 -0500

    Checkpointing progress on reimplementing the cube UI using part builder.

--- simulation/cube_ui.h
+++ simulation/cube_ui.h
@@ -9,6 +9,7 @@
 #include "graphics/cylinder.h"
 #include "graphics/model.h"
 #include "graphics/offset.h"
+#include "graphics/part_builder.h"
 #include "graphics/revolute_joint.h"
 #include "graphics/shader_program.h"
 #include "graphics/sphere.h"
@@ -45,33 +46,22 @@ class CubeScene final : public ui::Scene {
 
  public:
   CubeScene(GLFWwindow& window) : Scene("3D Cube"), model_(aspect_ratio(window)) {
-    std::unique_ptr<graphics::Box<GeometryType>> cube_sat{
-        std::make_unique<graphics::Box<GeometryType>>(.3, .3, .3)};
-
-    std::unique_ptr<graphics::RevoluteJoint<GeometryType>> joint1{
-        std::make_unique<graphics::RevoluteJoint<GeometryType>>()};
-    joint1_ = joint1.get();
-    joint1->set_rotation_axis(GeometryType::z_axis);
-    joint1->set_rotation_angle(pi);
-    joint1->set_offset(4);
-    joint1->set_offset_direction(GeometryType::y_axis);
-    joint1->add_element(std::move(cube_sat));
-
-    // std::unique_ptr<graphics::Sphere<GeometryType, 4>> object1{
-    //     std::make_unique<graphics::Sphere<GeometryType, 4>>()};
-
-    std::unique_ptr<graphics::Cylinder<GeometryType>> rod{
-        std::make_unique<graphics::Cylinder<GeometryType>>(.7f, .2f)};
-
-    std::unique_ptr<graphics::RevoluteJoint<GeometryType>> joint0{
-        std::make_unique<graphics::RevoluteJoint<GeometryType>>()};
-    joint0_ = joint0.get();
-    joint0->set_rotation_axis(GeometryType::y_axis);
-    joint0->set_rotation_angle(pi);
-    joint0->add_element(std::move(rod));
-    joint0->add_element(std::move(joint1));
-
-    model_.add_element(std::move(joint0));
+    graphics::PartBuilder<GeometryType> builder{};
+    builder.add_revolute_joint(GeometryType::y_axis, pi)
+        .bind(&joint0_)
+        .down()
+        .add_revolute_joint(GeometryType::z_axis)
+        .bind(&joint1_)
+        .down()
+        .add_sphere(.25)
+        .add_offset(14, GeometryType::y_axis)
+        .down()
+        .add_rod(0.25, 1, GeometryType::y_axis)
+        .down()
+        .add_offset(1, GeometryType::y_axis)
+        .down()
+        .add_cube(.25);
+    model_.add_element(builder.build());
   }
 
   void handle_loading() override {
@@ -85,8 +75,8 @@ class CubeScene final : public ui::Scene {
     using std::cos;
     using std::sin;
     const TimeType t{static_cast<TimeType>(ImGui::GetTime())};
-    joint0_->set_rotation_angle(pi * t / 8);
-    joint1_->set_rotation_angle(pi + pi / 3 * cos(pi / 2 * t));
+    // joint0_->set_rotation_angle(pi * t / 2);
+    // joint1_->set_rotation_angle(pi + pi / 3 * cos(pi / 2 * t));
   }
 
   void update_ui() override {}
