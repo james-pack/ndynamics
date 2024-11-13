b485711ceb453af8915aefca72f0e3d791831b51
James Pack <jgrimm@gmail.com>
2024-11-13 13:10:26 -0500

    Fixed null termination bug in read_file() function

--- io/utils.cc
+++ io/utils.cc
@@ -7,20 +7,19 @@
 namespace ndyn::io {
 
 std::string read_file(std::filesystem::path path) {
+  using std::to_string;
   std::string contents{};
 
   static constexpr size_t BUFFER_SIZE{1024};
   char buffer[BUFFER_SIZE];
   std::ifstream file{path, std::ios::binary | std::ios_base::in};
 
-  size_t bytes_read{BUFFER_SIZE};
-  while (bytes_read == BUFFER_SIZE) {
-    file.read(buffer, BUFFER_SIZE);
+  size_t bytes_read{BUFFER_SIZE - 1};
+  while (bytes_read == BUFFER_SIZE - 1) {
+    file.read(buffer, BUFFER_SIZE - 1);
     bytes_read = file.gcount();
-    if (bytes_read < BUFFER_SIZE) {
-      // The file.read() call doesn't null-terminate its output in binary mode.
-      buffer[bytes_read] = '\0';
-    }
+    // The file.read() call doesn't null-terminate its output in binary mode.
+    buffer[bytes_read] = '\0';
     contents.append(buffer);
   }
 
