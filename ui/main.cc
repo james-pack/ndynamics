f0ab7fffe8f212467798c16bb2ed2605ac719eff
James Pack <jgrimm@gmail.com>
2026-01-06 08:42:36 -0500

    Integrate a trivial use of the interpreter that will eventually evolve into the REPL.

--- ui/main.cc
+++ ui/main.cc
@@ -1,6 +1,8 @@
 #include "base/initializer.h"
 #include "glog/logging.h"
+#include "math/algebra.h"
 #include "ui/algebra_options.h"
+#include "ui/interpreter.h"
 
 using namespace ndyn::ui;
 
@@ -10,8 +12,14 @@ int main(int argc, char* argv[]) {
   const auto algebra{get_algebra_flag()};
   if (algebra == Algebras::Euclid3d) {
     LOG(INFO) << "Euclid 3D";
+    Interpreter<ndyn::math::Vga<>> interpreter{};
+    auto result{interpreter.eval("e1 * e2 + 1 + e1")};
+    LOG(INFO) << result;
   } else if (algebra == Algebras::Euclid2d) {
     LOG(INFO) << "Euclid 2D";
+    Interpreter<ndyn::math::Vga2d<>> interpreter{};
+    auto result{interpreter.eval("e1 * e2 + 1 + e1")};
+    LOG(INFO) << result;
   }
   return 0;
 }
