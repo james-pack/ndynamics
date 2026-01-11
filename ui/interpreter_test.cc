9af6fcc377feab641c0549738b2eb2686aa53061
James Pack <jgrimm@gmail.com>
2026-01-11 11:59:19 -0500

    Add overload of MatchesValue() to make tests easier to read.

--- ui/interpreter_test.cc
+++ ui/interpreter_test.cc
@@ -38,28 +38,34 @@ template <typename AlgebraT>
   return AreNear(interpreter.current_value, expected);
 }
 
-TEST(InterpreterTest, CanInterpretSimpleScalarExpression) {
+template <typename AlgebraT>
+::testing::AssertionResult MatchesValue(const std::string_view line,
+                                        const typename AlgebraT::ScalarType& expected) {
+  return MatchesValue<AlgebraT>(line, typename AlgebraT::VectorType{expected});
+}
+
+TEST(InterpreterTest, CanInterpretSimpleScalarExpressions) {
   using AlgebraType = Scalar<>;
-  EXPECT_TRUE(MatchesValue<AlgebraType>("1", AlgebraType::VectorType{1}));
-  EXPECT_TRUE(MatchesValue<AlgebraType>("1 * 2", AlgebraType::VectorType{2}));
-  EXPECT_TRUE(MatchesValue<AlgebraType>("2 * 2", AlgebraType::VectorType{4}));
-  EXPECT_TRUE(MatchesValue<AlgebraType>("1 + 2", AlgebraType::VectorType{3}));
-  EXPECT_TRUE(MatchesValue<AlgebraType>("2 + 2", AlgebraType::VectorType{4}));
-  EXPECT_TRUE(MatchesValue<AlgebraType>("2 - 2", AlgebraType::VectorType{0}));
-  EXPECT_TRUE(MatchesValue<AlgebraType>("4 - 2", AlgebraType::VectorType{2}));
+  EXPECT_TRUE(MatchesValue<AlgebraType>("1", 1));
+  EXPECT_TRUE(MatchesValue<AlgebraType>("1 * 2", 2));
+  EXPECT_TRUE(MatchesValue<AlgebraType>("2 * 2", 4));
+  EXPECT_TRUE(MatchesValue<AlgebraType>("1 + 2", 3));
+  EXPECT_TRUE(MatchesValue<AlgebraType>("2 + 2", 4));
+  EXPECT_TRUE(MatchesValue<AlgebraType>("2 - 2", 0));
+  EXPECT_TRUE(MatchesValue<AlgebraType>("4 - 2", 2));
 }
 
 // Note: this test catches a particular bug in the parsing where a '+' or a '-' without spaces gets
 // bound as a unary operator when it was intended to be the binary operator. Multiplication is then
 // assumed by the parser to be the binary operator between these operands.
-TEST(InterpreterTest, CanInterpretSimpleScalarExpressionWithoutSeparators) {
+TEST(InterpreterTest, CanInterpretSimpleScalarExpressionsWithoutSeparators) {
   using AlgebraType = Scalar<>;
-  EXPECT_TRUE(MatchesValue<AlgebraType>("1*2", AlgebraType::VectorType{2}));
-  EXPECT_TRUE(MatchesValue<AlgebraType>("2*2", AlgebraType::VectorType{4}));
-  EXPECT_TRUE(MatchesValue<AlgebraType>("1+2", AlgebraType::VectorType{3}));
-  EXPECT_TRUE(MatchesValue<AlgebraType>("2+2", AlgebraType::VectorType{4}));
-  EXPECT_TRUE(MatchesValue<AlgebraType>("2-2", AlgebraType::VectorType{0}));
-  EXPECT_TRUE(MatchesValue<AlgebraType>("4-2", AlgebraType::VectorType{2}));
+  EXPECT_TRUE(MatchesValue<AlgebraType>("1*2", 2));
+  EXPECT_TRUE(MatchesValue<AlgebraType>("2*2", 4));
+  EXPECT_TRUE(MatchesValue<AlgebraType>("1+2", 3));
+  EXPECT_TRUE(MatchesValue<AlgebraType>("2+2", 4));
+  EXPECT_TRUE(MatchesValue<AlgebraType>("2-2", 0));
+  EXPECT_TRUE(MatchesValue<AlgebraType>("4-2", 2));
 }
 
 TEST(InterpreterTest, CanInterpretComplexBasisVector) {
@@ -67,34 +73,48 @@ TEST(InterpreterTest, CanInterpretComplexBasisVector) {
   EXPECT_TRUE(MatchesValue<AlgebraType>("i", AlgebraType::VectorType::e<0>()));
 }
 
+TEST(InterpreterTest, CanInterpretExpressionsInComplexAlgebra) {
+  using AlgebraType = Complex<>;
+
+  constexpr auto i{AlgebraType::VectorType::e<0>()};
+
+  EXPECT_TRUE(MatchesValue<AlgebraType>("1", 1));
+  EXPECT_TRUE(MatchesValue<AlgebraType>("1 * 2", 2));
+  EXPECT_TRUE(MatchesValue<AlgebraType>("2 * 2", 4));
+  EXPECT_TRUE(MatchesValue<AlgebraType>("1 + 2", 3));
+  EXPECT_TRUE(MatchesValue<AlgebraType>("2 + 2", 4));
+  EXPECT_TRUE(MatchesValue<AlgebraType>("2 - 2", 0));
+  EXPECT_TRUE(MatchesValue<AlgebraType>("4 - 2", 2));
+
+  EXPECT_TRUE(MatchesValue<AlgebraType>("i", i));
+  EXPECT_TRUE(MatchesValue<AlgebraType>("1 * i", i));
+  EXPECT_TRUE(MatchesValue<AlgebraType>("i * 1", i));
+  EXPECT_TRUE(MatchesValue<AlgebraType>("i * 2", 2 * i));
+  EXPECT_TRUE(MatchesValue<AlgebraType>("i * i", i * i));
+  EXPECT_TRUE(MatchesValue<AlgebraType>("i * i", -1));
+}
+
 TEST(InterpreterTest, CanInterpretVga2dBasisVectors) {
   using AlgebraType = Vga2d<>;
-  EXPECT_TRUE(MatchesValue<AlgebraType>("e1", AlgebraType::VectorType::e<0>()));
-  EXPECT_TRUE(MatchesValue<AlgebraType>("e2", AlgebraType::VectorType::e<1>()));
-  EXPECT_TRUE(
-      MatchesValue<AlgebraType>("e12",  //
-                                AlgebraType::VectorType::e<0>() * AlgebraType::VectorType::e<1>()));
+  constexpr auto e1{AlgebraType::VectorType::e<0>()};
+  constexpr auto e2{AlgebraType::VectorType::e<1>()};
+  EXPECT_TRUE(MatchesValue<AlgebraType>("e1", e1));
+  EXPECT_TRUE(MatchesValue<AlgebraType>("e2", e2));
+  EXPECT_TRUE(MatchesValue<AlgebraType>("e12", e1 * e2));
 }
 
 TEST(InterpreterTest, CanInterpretVgaBasisVectors) {
   using AlgebraType = Vga<>;
-  EXPECT_TRUE(MatchesValue<AlgebraType>("e1", AlgebraType::VectorType::e<0>()));
-  EXPECT_TRUE(MatchesValue<AlgebraType>("e2", AlgebraType::VectorType::e<1>()));
-  EXPECT_TRUE(MatchesValue<AlgebraType>("e3", AlgebraType::VectorType::e<2>()));
-  EXPECT_TRUE(
-      MatchesValue<AlgebraType>("e12",  //
-                                AlgebraType::VectorType::e<0>() * AlgebraType::VectorType::e<1>()));
-  EXPECT_TRUE(
-      MatchesValue<AlgebraType>("e13",  //
-                                AlgebraType::VectorType::e<0>() * AlgebraType::VectorType::e<2>()));
-  EXPECT_TRUE(
-      MatchesValue<AlgebraType>("e23",  //
-                                AlgebraType::VectorType::e<1>() * AlgebraType::VectorType::e<2>()));
-
-  EXPECT_TRUE(
-      MatchesValue<AlgebraType>("e123",  //
-                                AlgebraType::VectorType::e<0>() * AlgebraType::VectorType::e<1>() *
-                                    AlgebraType::VectorType::e<2>()));
+  constexpr auto e1{AlgebraType::VectorType::e<0>()};
+  constexpr auto e2{AlgebraType::VectorType::e<1>()};
+  constexpr auto e3{AlgebraType::VectorType::e<2>()};
+  EXPECT_TRUE(MatchesValue<AlgebraType>("e1", e1));
+  EXPECT_TRUE(MatchesValue<AlgebraType>("e2", e2));
+  EXPECT_TRUE(MatchesValue<AlgebraType>("e3", e3));
+  EXPECT_TRUE(MatchesValue<AlgebraType>("e12", e1 * e2));
+  EXPECT_TRUE(MatchesValue<AlgebraType>("e13", e1 * e3));
+  EXPECT_TRUE(MatchesValue<AlgebraType>("e23", e2 * e3));
+  EXPECT_TRUE(MatchesValue<AlgebraType>("e123", e1 * e2 * e3));
 }
 
 }  // namespace ndyn::ui
