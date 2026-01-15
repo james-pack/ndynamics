a0a728e9c6b2f53cc07bb23063fa737fe6290880
James Pack <jgrimm@gmail.com>
2026-01-15 10:54:04 -0500

    Change RenderBackend to snake_case method and variable names.

--- gfx/render_backend.h
+++ gfx/render_backend.h
@@ -15,21 +15,21 @@ class RenderBackend {
  public:
   virtual ~RenderBackend() = default;
 
-  virtual std::unique_ptr<ShaderModule> createVertexShader(const void* bytecode,
-                                                           std::size_t size) = 0;
-
-  virtual std::unique_ptr<ShaderModule> createFragmentShader(const void* bytecode,
+  virtual std::unique_ptr<ShaderModule> create_vertex_shader(const void* bytecode,
                                                              std::size_t size) = 0;
 
-  virtual std::unique_ptr<RenderPipeline> createRenderPipeline(ShaderModule& vertex,
-                                                               ShaderModule& fragment,
-                                                               PipelineLayout& layout,
-                                                               RenderPass& pass) = 0;
+  virtual std::unique_ptr<ShaderModule> create_fragment_shader(const void* bytecode,
+                                                               std::size_t size) = 0;
 
-  virtual std::unique_ptr<CommandBuffer> createCommandBuffer() = 0;
+  virtual std::unique_ptr<RenderPipeline> create_render_pipeline(ShaderModule& vertex,
+                                                                 ShaderModule& fragment,
+                                                                 PipelineLayout& layout,
+                                                                 RenderPass& pass) = 0;
 
-  virtual void draw(CommandBuffer& cmd, RenderPipeline& pipeline, Buffer& vertexBuffer,
-                    Buffer* indexBuffer, std::uint32_t vertexOrIndexCount) = 0;
+  virtual std::unique_ptr<CommandBuffer> create_command_buffer() = 0;
+
+  virtual void dispatch(CommandBuffer& cmd, RenderPipeline& pipeline, Buffer& vertex_buffer,
+                        Buffer* index_buffer, std::uint32_t vertexOrIndexCount) = 0;
 
   virtual std::unique_ptr<Fence> submit(CommandBuffer& cmd) = 0;
 };
