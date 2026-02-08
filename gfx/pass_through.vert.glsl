9476a0d5688d04d79c0f107cb8f3815929099f14
James Pack <jgrimm@gmail.com>
2026-02-08 12:18:07 -0500

    Update formatting of a vertex shader to better conform to the project style.

--- gfx/pass_through.vert.glsl
+++ gfx/pass_through.vert.glsl
@@ -3,13 +3,9 @@
 layout(location = 0) in vec3 in_pos;
 layout(location = 1) in vec3 in_normal;
 
-layout(std430, set = 0, binding = 0) readonly buffer InstanceTransforms
-{
-    mat4 model[];
-};
+layout(std430, set = 0, binding = 0) readonly buffer InstanceTransforms { mat4 model[]; };
 
-void main()
-{
-    mat4 M = model[gl_InstanceIndex];
-    gl_Position = M * vec4(in_pos, 1.0);
+void main() {
+  mat4 M = model[gl_InstanceIndex];
+  gl_Position = M * vec4(in_pos, 1.0);
 }
