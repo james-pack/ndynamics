4e5ecae5dc9409b1beb0ba5cd8a09d198aa8a0a2
James Pack <jgrimm@gmail.com>
2026-02-10 16:32:43 -0500

    Add a second cube and additional colors as materials.

--- gfx/render_cube.cc
+++ gfx/render_cube.cc
@@ -3,27 +3,44 @@
 #include <thread>
 
 #include "base/initializer.h"
+#include "gfx/material.h"
 #include "gfx/math.h"
 #include "gfx/mesh.h"
 #include "gfx/vulkan_renderer.h"
+#include "glog/logging.h"
 
 using namespace ndyn::gfx;
+using namespace std::chrono_literals;
 
 namespace {
 
 Quat axis_angle(const Vec3& axis, float angle) {
-  const float s{std::sin(angle * 0.5f)};
-  return Quat{std::cos(angle * 0.5f), axis.x * s, axis.y * s, axis.z * s}.normalize();
+  const float s{std::sin(0.5f * angle)};
+  return Quat{std::cos(0.5f * angle), s * axis.x, s * axis.y, s * axis.z}.normalize();
 }
 
 }  // namespace
+
+static constexpr Material BLUE{.diffuse_color = {0.f, 0.f, 1.f, 1.f},
+                               .specular_color = {1.f, 1.f, 1.f, 1.f}};
+
+static constexpr Material GREEN{.diffuse_color = {0.f, 1.f, 0.f, 1.f},
+                                .specular_color = {1.f, 1.f, 1.f, 1.f}};
 
 int main(int argc, char* argv[]) {
   ndyn::initialize(&argc, &argv);
 
   VulkanRenderer renderer{};
-  const MeshId mesh{renderer.add_mesh(create_cube(0.2f))};
-  const InstanceId id1{renderer.add_instance(Instance{mesh})};
+
+  const MeshId mesh{renderer.add_mesh(create_cube(0.7f))};
+  const MaterialId blue{renderer.add_material(BLUE)};
+  const MaterialId green{renderer.add_material(GREEN)};
+
+  const InstanceId instance1{
+      renderer.add_instance(Instance{Mat4::identity(), BLUE.diffuse_color, mesh, blue})};
+
+  const InstanceId instance2{
+      renderer.add_instance(Instance{Mat4::identity(), GREEN.diffuse_color, mesh, green})};
 
   // {
   //   Camera camera{};
@@ -31,17 +48,26 @@ int main(int argc, char* argv[]) {
   //   renderer.set_camera(camera);
   // }
 
+  Transform instance1_position{};
+  Transform instance2_position{};
   const auto start{std::chrono::high_resolution_clock::now()};
 
   while (true) {
     const auto now{std::chrono::high_resolution_clock::now()};
     float t = std::chrono::duration<float>(now - start).count();
 
-    Transform position{};
-    position.position = {std::sin(t) * 2.f, std::sin(t * 0.5f) * 1.f, 0.f};
-    position.rotation = axis_angle({1.f, 1.f, 0.f}, 0.1f * t);
+    instance1_position.position = {-0.2f, -0.2f, -0.2f};
+    instance1_position.rotation = axis_angle({0.f, 0.f, 1.f}, 0.1f * t);
+    Mat4 model_matrix1{create_model_matrix(instance1_position)};
+    renderer.update_position(instance1, model_matrix1);
 
-    renderer.update_position(id1, position);
+    instance2_position.position = {0.5f, 0.5f, 0.5f};
+    instance2_position.rotation = axis_angle({0.f, 0.f, 1.f}, 0.1f * t);
+    Mat4 model_matrix2{create_model_matrix(instance2_position)};
+    renderer.update_position(instance2, model_matrix2);
+
     renderer.render_frame();
+
+    std::this_thread::sleep_for(5ms);
   }
 }
