fe5d5ce1635fb84b9ea4f66fffba729c5cfb0ba8
James Pack <jgrimm@gmail.com>
2026-02-12 14:14:13 -0500

    Add a transform to adapt the model coordinate cube to Vulkan's coordinate system.

--- gfx/model.vert.glsl
+++ gfx/model.vert.glsl
@@ -12,8 +12,21 @@ layout(location = 1) in vec3 in_normal;
 
 layout(location = 0) out flat uint material_id;
 
+// Matrix to transform model-space coordinates into Vulkan view space.
+// Model coordinates x,y,z on [-1,1] with +x pointing to the right, +y pointing up, and +z pointing
+// out of the screen. Vulkan coordinates for the x and y axes are the same, but Vulkan has the z
+// axis on [0,1] with +z pointing into the screen. This transform flips and scales the z axis and
+// translates it to achieve the desired coordinate system.
+// Also, the y-axis is flipped to align Vulkan’s NDC with the windowing system's coordinates.
+const mat4 model_to_view = mat4(  //
+    1.0, 0.0, 0.0, 0.0,           //
+    0.0, -1.0, 0.0, 0.0,          //
+    0.0, 0.0, -0.5, 0.0,          //
+    0.0, 0.0, 0.5, 1.0            //
+);
+
 void main() {
-  mat4 M = instances[gl_InstanceIndex].position;
-  gl_Position = M * vec4(in_pos, 1.0);
+  mat4 model_position_orientation = instances[gl_InstanceIndex].position;
+  gl_Position = model_to_view * model_position_orientation * vec4(in_pos, 1.0);
   material_id = instances[gl_InstanceIndex].material_id;
 }
