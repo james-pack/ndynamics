510ed297038f7ded2c42a5eef6fa94e3a72e7b59
James Pack <jgrimm@gmail.com>
2026-02-12 14:15:13 -0500

    Make the per-instance and time-based position and orientation easier to comprehend.

--- gfx/render_cube.cc
+++ gfx/render_cube.cc
@@ -1,6 +1,7 @@
 #include <array>
 #include <chrono>
 #include <cmath>
+#include <numbers>
 #include <thread>
 
 #include "base/initializer.h"
@@ -16,14 +17,7 @@ DEFINE_uint32(num_cubes, 3, "Number of cubes to display.");
 using namespace ndyn::gfx;
 using namespace std::chrono_literals;
 
-namespace {
-
-Quat axis_angle(const Vec3& axis, float angle) {
-  const float s{std::sin(0.5f * angle)};
-  return Quat{std::cos(0.5f * angle), s * axis.x, s * axis.y, s * axis.z}.normalize();
-}
-
-}  // namespace
+namespace {}  // namespace
 
 static constexpr Material RED{.diffuse_color = {1.f, 0.f, 0.f, 1.f},
                               .specular_color = {1.f, 1.f, 1.f, 1.f}};
@@ -48,7 +42,8 @@ int main(int argc, char* argv[]) {
 
   VulkanRenderer renderer{};
 
-  const MeshId mesh{renderer.add_mesh(create_cube(0.5f))};
+  const MeshId mesh{renderer.add_mesh(create_cube(0.2f))};
+  // const MeshId mesh{renderer.add_mesh(create_cylinder(0.2f, 0.5f, 8))};
 
   std::array materials{
       renderer.add_material(CYAN), renderer.add_material(MAGENTA), renderer.add_material(YELLOW),
@@ -62,22 +57,17 @@ int main(int argc, char* argv[]) {
         renderer.add_instance(Instance{Mat4::identity(), mesh, materials[i % materials.size()]}));
   }
 
-  // {
-  //   Camera camera{};
-  //   camera.position = {0.f, 0.f, 5.f};
-  //   renderer.set_camera(camera);
-  // }
-
   const auto start{std::chrono::high_resolution_clock::now()};
-
   while (true) {
     const auto now{std::chrono::high_resolution_clock::now()};
     float t = std::chrono::duration<float>(now - start).count();
 
     for (size_t i = 0; i < FLAGS_num_cubes; ++i) {
       Position instance_position{};
-      instance_position.position = {-2.8f + 1.7f * i, -0.8f + 0.7f * i, 0.2f + 1.7f * i};
-      instance_position.orientation = axis_angle({1.f, 0.f, 1.f}, 0.1f * t);
+      instance_position.position = {1.f * std::cos(t + i / 5.f),  //
+                                    1.f * std::sin(t + i / 5.f),  //
+                                    0.5f - 1.f * i};
+      instance_position.orientation = axis_angle({1.f, 0.f, 1.f}, M_PI / 4);
       Mat4 model_matrix{create_model_matrix(instance_position)};
       renderer.update_position(instances[i], model_matrix);
     }
